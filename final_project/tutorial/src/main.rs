fc9503837f74aad3c8533043fecf4ccc2fd9e1f6
Yves Callaert <yves_callaert@hotmail.com>
2023-02-22 14:42:36 +0000

    Add collision detection

--- final_project/tutorial/src/main.rs
+++ final_project/tutorial/src/main.rs
@@ -16,14 +16,18 @@ fn main(){
     let mut game = Game::new();
 
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
-    player.translation = Vec2::new(200.0, 100.0);
+    player.translation = Vec2::new(0.0, 0.0);
     player.rotation = UP; //you can choose conventient direction or pi fractals eg: std::f32::consts::FRAC_2_PI;
     player.scale = 1.0; //number * 100
     player.layer = 1.0;
+    player.collision = true;
 
     // let temporary = game.add_sprite("player", SpritePreset::RacingCarRed);
     // temporary.translation = Vec2::new(30.0, 0.0);
     // temporary.layer = 0.0;
+    let car1 = game.add_sprite("player", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
 
@@ -31,6 +35,22 @@ fn main(){
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState){
-    game_state.current_score += 1;
-    println!("The current score is {}", game_state.current_score);
+    //engine.show_colliders = true; shows the polygon outline of a sprite
+
+    for event in engine.collision_events.drain(..){
+        if event.state == CollisionState::Begin && event.pair.one_starts_with("player"){
+            //remove the sprite the player collided with
+            for label in [event.pair.0, event.pair.1] {
+                if label != "player"{
+                    engine.sprites.remove(&label);
+                }
+            }
+            game_state.current_score += 1 ;
+            println!("The current score is {}", game_state.current_score);
+        }
+    }
+
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
+
 }
